736f29bc4a12aff3dbefed92adbb6d6dff0ec1db
Anatoli Voronov <anatoli-voronov@yandex.ru>
2022-10-20 02:09:15 +0300

    add Calc_2

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 num_1 = float(input('Введите первое число: '))
-
-operation = input('''
-Введите арифметическое действие:
-+ сложение
-- вычитание
-* умножение
-/ деление
-** Возведение в степень
-''')
+operation = input('''Введите арифметическое действие: + ,- ,* ,/ ,** : ''')
 num_2 = float(input('Введите второе число: '))
 
 
@@ -27,3 +19,13 @@ def calculate():
 
 
 calculate()
+calc_again = input('Хотите продолжить нажмите Y если нет то N ')
+
+
+def again():
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
