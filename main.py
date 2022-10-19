d57319ffcd2d14ca36b321f08d57a273fb6c87ec
Anatoli Voronov <anatoli-voronov@yandex.ru>
2022-10-19 23:47:03 +0300

    add Calc

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("hello calculator python")
+num_1 = float(input('Введите первое число: '))
+
+operation = input('''
+Введите арифметическое действие:
++ сложение
+- вычитание
+* умножение
+/ деление
+** Возведение в степень
+''')
+num_2 = float(input('Введите второе число: '))
+
+
+def calculate():
+    if operation == '+':
+        print(f' {num_1} + {num_2} = {num_1 + num_2}')
+    elif operation == "-":
+        print(f' {num_1} - {num_2} = {num_1 - num_2}')
+    elif operation == '*':
+        print(f' {num_1} * {num_2} = {num_1 * num_2}')
+    elif operation == '/':
+        print(f' {num_1} / {num_2} = {num_1 / num_2}')
+    elif operation == '**':
+        print(f' {num_1} ** {num_2} = {num_1 ** num_2}')
+    else:
+        print('не правильное действие ')
+
+
+calculate()
